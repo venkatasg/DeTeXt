f3c7f2ccde14382c0f50ce813c61d25201be3375
venkatasg <gvenkata1994@gmail.com>
2020-08-29 11:08:47 -0500

    Initial Commit

---
+++ Shared/DeTeXtApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeTeXtApp.swift
+//  Shared
+//
+//  Created by Venkat on 29/8/20.
+//
+
+import SwiftUI
+
+@main
+struct DeTeXtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
