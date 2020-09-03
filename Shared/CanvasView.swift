c51c6423bd1484bd587bedc08ac34001123fa7cb
venkatasg <gvenkata1994@gmail.com>
2020-09-03 09:56:42 -0500

    Neuromorphic extension, average color calculation to flip color to white on black when in light mode

--- Shared/CanvasView.swift
+++ Shared/CanvasView.swift
@@ -22,26 +22,34 @@ struct CanvasView: View {
     var body: some View {
         NavigationView {
             VStack{
-                PKCanvas(canvasView: $canvas).aspectRatio(1, contentMode: .fit).cornerRadius(25).overlay(
-                    RoundedRectangle(cornerRadius: 25)
-                    .stroke(lineWidth: 5) .fill((colorScheme == .light ? Color.black : Color.white))
-                ).padding(16)
+                PKCanvas(canvasView: $canvas)
+                    .aspectRatio(1.5, contentMode: .fit)
+                    .cornerRadius(15)
+                    .overlay(RoundedRectangle(cornerRadius: 25)
+                    .stroke(lineWidth: 0)
+                    .fill((colorScheme == .light ? Color.black : Color.white)))
+                    .padding(16)
+//                    .background(RoundedRectangle(cornerRadius: 10)
+//                    .fill(Color.neuBackground))
+                    .shadow(color: .dropShadow, radius: 15, x: 10, y: 10)
+                    .shadow(color: .dropShadow, radius: 15, x: 10, y: -10)
+                    .shadow(color: .dropShadow, radius: 15, x: -10, y: 10)
+                    .shadow(color: .dropShadow, radius: 15, x: -10, y: -10)
+                    .foregroundColor(.primary)
                 Spacer()
                 Text(self.predictionOne).padding(8)
                 Text(self.predictionTwo).padding(8)
                 Text(self.predictionThree).padding(8)
                 Spacer()
             }
-
+                .background((colorScheme == .light ? Color.neuBackground : Color.neuBackgroundDark))
                 .navigationBarItems(leading: Button(action: { self.canvas.drawing = PKDrawing() }) {
                         Text("Clear")
-//                            .font(.body)
                             .padding(8)
                     },
                 trailing:
                     Button(action: {self.showAboutView.toggle()}) {
                         Text("About")
-//                            .font(.body)
                             .padding(8)
                     }
                 )
@@ -57,9 +65,8 @@ struct PKCanvas: UIViewRepresentable {
         var pkCanvas: PKCanvas
 //        let compiledUrl = try! MLModel.compileModel(at: URL(fileURLWithPath: "deTeX.mlmodel"))
 //        let model = try! MLModel(contentsOf: compiledUrl)
-        
+//        @Environment(\.colorScheme) var colorScheme: ColorScheme
         let model = deTeX()
-        @Environment(\.colorScheme) var colorScheme
         private let trainedImageSize = CGSize(width: 300, height: 200)
         let symbols = loadJson()
         
@@ -68,12 +75,11 @@ struct PKCanvas: UIViewRepresentable {
         }
         
         func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
-            
             // if canvasView is empty escape gracefully
             if canvasView.drawing.bounds.isEmpty { }
             else {
                 var image = canvasView.drawing.image(from: canvasView.drawing.bounds, scale: 5.0)
-                if self.colorScheme == .light {
+                if image.averageColor?.cgColor.components![0] == 0 {
                     image = invertColors(image: image)
                 }
                 let processed_image = preprocessImage(image: image)
@@ -109,33 +115,33 @@ struct PKCanvas: UIViewRepresentable {
                         return
                     }
                 let sortedClassProbs = result.classLabelProbs.sorted { $0.1 > $1.1 }
-                print(symbols!.first(where: {$0.id==sortedClassProbs[0].key})?.command ?? "None", sortedClassProbs[0].value,
-                      symbols!.first(where: {$0.id==sortedClassProbs[0].key})?.mathmode ?? "None",
-                      symbols!.first(where: {$0.id==sortedClassProbs[0].key})?.textmode ?? "None",
-                      symbols!.first(where: {$0.id==sortedClassProbs[0].key})?.package ?? "None",
-                      symbols!.first(where: {$0.id==sortedClassProbs[0].key})?.fontenc ?? "None")
+                for i in 0 ..< 3 {
+                print(symbols!.first(where: {$0.id==sortedClassProbs[i].key})?.command ?? "None",
+                      sortedClassProbs[i].value,
+                      symbols!.first(where: {$0.id==sortedClassProbs[i].key})?.mathmode ?? "None",
+                      symbols!.first(where: {$0.id==sortedClassProbs[i].key})?.textmode ?? "None",
+                      symbols!.first(where: {$0.id==sortedClassProbs[i].key})?.package ?? "None",
+                      symbols!.first(where: {$0.id==sortedClassProbs[i].key})?.fontenc ?? "None")
+                }
             }
         }
     }
 
     @Binding var canvasView: PKCanvasView
-    @Environment(\.colorScheme) var colorScheme
-    
-    
+//    @Environment(\.colorScheme) var colorScheme
 
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
     func makeUIView(context: Context) -> PKCanvasView {
-        self.canvasView.backgroundColor = UIColor.systemBackground
-        self.canvasView.tool = PKInkingTool(.pen, color: (self.colorScheme == .light ? .black : .white), width: 10)
+//        self.canvasView.backgroundColor = UIColor.systemBackground
+        self.canvasView.tool = PKInkingTool(.pen, width: 15)
         self.canvasView.isScrollEnabled = false
         self.canvasView.becomeFirstResponder()
         self.canvasView.delegate = context.coordinator
         
         if #available(iOS 14.0, *) {
-            // Both finger and pencil are always allowed on this canvas.
             self.canvasView.drawingPolicy = .anyInput
         }
         else {
@@ -146,14 +152,17 @@ struct PKCanvas: UIViewRepresentable {
     }
 
     func updateUIView(_ canvasView: PKCanvasView, context: Context) {
-        self.canvasView.backgroundColor = UIColor.systemBackground
-//        self.canvasView.tool = PKInkingTool(.pen, color: (self.colorScheme == .light ? .black : .blue), width: 10)
+//        self.canvasView.backgroundColor = UIColor.systemBackground
+        self.canvasView.tool = PKInkingTool(.pen, width: 15)
     }
 }
 
 struct CanvasView_Previews: PreviewProvider {
     static var previews: some View {
-        CanvasView()
-            .previewDevice("iPhone 11 Pro Max")
+        Group {
+            CanvasView()
+                .preferredColorScheme(.light)
+                .previewDevice("iPhone 11 Pro Max")
+        }
     }
 }
