c5523dd5ae9b5b21d61b934a13a896c142056793
venkatasg <gvenkata1994@gmail.com>
2020-10-03 14:34:53 -0500

    Code reorganization
    on the road to modularity

--- Shared/DeTeXtApp.swift
+++ Shared/DeTeXtApp.swift
@@ -14,58 +14,14 @@ struct DeTeXtApp: App {
     @StateObject var symbols = Symbols()
 
     var body: some Scene {
+        #if os(iOS)
         WindowGroup {
             MainView()
                 .environmentObject(symbols)
         }
+        #endif
     }
 }
 
-struct MainView: View {
-    
-    @State private var selection: String = "draw"
-    @EnvironmentObject var symbols: Symbols
-    
-    var body: some View {
-        TabView(selection: $selection) {
-            CanvasView()
-                .environmentObject(symbols)
-                .tabItem {
-                    Image(systemName: "scribble")
-                        .accessibility(label: Text("Draw symbols."))
-                    Text("Draw")
-                }
-                .tag("draw")
-            SearchView()
-                .environmentObject(symbols)
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                        .accessibility(label: Text("Search symbols."))
-                        .accessibility(hint: Text("Search the entire list of 1098 LaTeX symbols by name."))
-                    Text("Symbols")
-                }
-                .tag("search")
-            AboutView()
-                .tabItem {
-                    Image(systemName: "questionmark.circle.fill")
-                        .accessibility(label: Text("About"))
-                    Text("About")
-                }
-                .tag("about")
-        }
-    }
-}
 
-struct MainView_Previews: PreviewProvider {
-    static let symbols = Symbols()
-    static var previews: some View {
-        Group {
-            MainView()
-                .previewLayout(.device)
-                .environmentObject(symbols)
-                .previewDevice("iPhone 11 Pro Max")
-                
-        }
-    }
-}
 
---
+++ iOS/MainView.swift
@@ -0,0 +1,56 @@
+//
+//  MainView.swift
+//  iOS
+//
+//  Created by Venkat on 3/10/20.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State private var selection: String = "draw"
+    @EnvironmentObject var symbols: Symbols
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            CanvasView()
+                .environmentObject(symbols)
+                .tabItem {
+                    Image(systemName: "scribble")
+                        .accessibility(label: Text("Draw symbols."))
+                    Text("Draw")
+                }
+                .tag("draw")
+            SearchView()
+                .environmentObject(symbols)
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                        .accessibility(label: Text("Search symbols."))
+                        .accessibility(hint: Text("Search the entire list of 1098 LaTeX symbols by name."))
+                    Text("Symbols")
+                }
+                .tag("search")
+            AboutView()
+                .tabItem {
+                    Image(systemName: "questionmark.circle.fill")
+                        .accessibility(label: Text("About"))
+                    Text("About")
+                }
+                .tag("about")
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static let symbols = Symbols()
+    static var previews: some View {
+        Group {
+            MainView()
+                .previewLayout(.device)
+                .environmentObject(symbols)
+                .previewDevice("iPhone 11 Pro Max")
+                
+        }
+    }
+}
