9c64de1776e188384eefbb75f940cb0846634718
venkatasg <gvenkata1994@gmail.com>
2020-10-02 20:23:37 -0500

    Refactored code to group 2 views together

--- Shared/RowView.swift
+++ Shared/RowView.swift
@@ -15,61 +15,51 @@ struct RowView: View {
     var confidence: Double?
     
     var body: some View {
-        if let conf = confidence {
-            HStack {
-                ZStack {
-                    Image("\(symbol.css_class)", label: Text(symbol.command))
-                        .font(.largeTitle)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:40, alignment: .center)
-                        .foregroundColor((colorScheme == .light ? Color.black : Color.white))
-                }
-                .frame(width:40, height:40, alignment: .leading)
-                Divider()
-                SymbolDetailsView(symbol: symbol)
-                Spacer()
-                Text(String(format: "%.1f", conf) + "%")
-                    .font(.system(.callout, design: .rounded))
-            }
-            .contextMenu(/*@START_MENU_TOKEN@*/ContextMenu(menuItems: {
-                Button(action: {
-                    let pasteboard = UIPasteboard.general
-                    pasteboard.string = symbol.command
-                }){
-                    HStack {
-                        Text("Copy command")
-                        Image(systemName: "doc.on.doc.fill")
+        Group {
+            if let conf = confidence {
+                HStack {
+                    ZStack {
+                        Image("\(symbol.css_class)", label: Text(symbol.command))
+                            .font(.largeTitle)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:40, alignment: .center)
+                            .foregroundColor((colorScheme == .light ? Color.black : Color.white))
                     }
+                    .frame(width:40, height:40, alignment: .leading)
+                    Divider()
+                    SymbolDetailsView(symbol: symbol)
+                    Spacer()
+                    Text(String(format: "%.1f", conf) + "%")
+                        .font(.system(.callout, design: .rounded))
                 }
-            })/*@END_MENU_TOKEN@*/)
-        }
-        
-        else {
-            HStack {
-                SymbolDetailsView(symbol: symbol)
-                Spacer()
-                ZStack {
-                    Image("\(symbol.css_class)", label: Text(symbol.command))
-                        .font(.largeTitle)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:40, alignment: .center)
-                        .foregroundColor((colorScheme == .light ? Color.black : Color.white))
-                }
-                .frame(width:40, height:40, alignment: .trailing)
-                
             }
-            .contextMenu(/*@START_MENU_TOKEN@*/ContextMenu(menuItems: {
-                Button(action: {
-                    let pasteboard = UIPasteboard.general
-                    pasteboard.string = symbol.command
-                }){
-                    HStack {
-                        Text("Copy command")
-                        Image(systemName: "doc.on.doc.fill")
+            else {
+                HStack {
+                    SymbolDetailsView(symbol: symbol)
+                    Spacer()
+                    ZStack {
+                        Image("\(symbol.css_class)", label: Text(symbol.command))
+                            .font(.largeTitle)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:40, alignment: .center)
+                            .foregroundColor((colorScheme == .light ? Color.black : Color.white))
                     }
+                    .frame(width:40, height:40, alignment: .trailing)
+                    
                 }
-            })/*@END_MENU_TOKEN@*/)
+            }
         }
+        .contextMenu(/*@START_MENU_TOKEN@*/ContextMenu(menuItems: {
+            Button(action: {
+                let pasteboard = UIPasteboard.general
+                pasteboard.string = symbol.command
+            }){
+                HStack {
+                    Text("Copy command")
+                    Image(systemName: "doc.on.doc.fill")
+                }
+            }
+        })/*@END_MENU_TOKEN@*/)
     }
 }
 
