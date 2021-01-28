a83e1e4842387e2c05ddd68adfeb4a861b10adae
Venkat <gvenkata1994@gmail.com>
2021-01-28 10:46:01 -0600

    Added help menu commands to mac catalyst target

--- Shared/DeTeXtApp.swift
+++ Shared/DeTeXtApp.swift
@@ -13,10 +13,38 @@ struct DeTeXtApp: App {
     @StateObject var symbols = Symbols()
 
     var body: some Scene {
+        #if targetEnvironment(macCatalyst)
+        WindowGroup {
+            SidebarView()
+                .environmentObject(symbols)
+            }
+        .commands {
+                    SidebarCommands()
+                    CommandGroup(replacing: .help, addition: {
+                        Link("Help and Support",
+                             destination: URL(string: "https://venkatasg.me/apps/detext#support")!)
+                        
+                        Divider()
+                        
+                        Link("Website",
+                             destination: URL(string: "https://venkatasg.me/apps/detext")!)
+                        Link("GitHub Repository",
+                             destination: URL(string: "https://github.com/venkatasg/DeTeXt")!)
+                        
+                        Divider()
+                        
+                        Link("Credits",
+                             destination: URL(string: "https://venkatasg.me/apps/detext#credits")!)
+                        Link("Version History",
+                             destination: URL(string: "https://venkatasg.me/apps/detext#version-history")!)
+                    })
+                }
+        #else
         WindowGroup {
             MainView()
                 .environmentObject(symbols)
-        }
+            }
+        #endif
     }
 }
 
