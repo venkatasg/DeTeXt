dc25a9e56ba6b34d289fb3ec3010ec54782e2ef5
Venkat <gvenkata1994@gmail.com>
2021-08-21 09:46:09 -0500

    Removed TipsView, merged info into AboutView

--- iOS/AboutView.swift
+++ iOS/AboutView.swift
@@ -12,12 +12,23 @@ struct AboutView: View {
     var body: some View {
         NavigationView {
             List {
+                Section(header: Text(appVersion()).padding(.leading, 20)) {
+                    Text("Made, with ❤️, by Venkat. Inspired by Detexify, I wanted to make a native iOS app for translating hand-drawn symbols to their corresponding LaTeX commands that was fast, efficient, and lightweight.")
+                }
+                
                 //Feedback
-                Section(header: HStack{
-                                        Image(systemName: "bubble.left.and.bubble.right")
-                                            .accessibility(label: Text("Feedback Section"))
-                                        Text("Feedback")}) {
-                    Text("You can contact me on Twitter for support, to report any bugs, or to suggest new features for the app.")
+                Section(header:Text("Feedback").padding(.leading, 20) ) {
+                    Text("You can email me or contact me on Twitter for support, to report any bugs, or to suggest new features for the app.")
+                    HStack {
+                        Image(systemName: "envelope.fill")
+                            .font(.body)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:20, alignment: .center)
+                            .foregroundColor(.blue)
+                            .accessibility(label: Text("Contact me on Twitter"))
+                        Link("Send me an email", destination: URL(string: "mailto:venkat@venkatasg.net")!)
+                            .foregroundColor(.primary)
+                    }
                     HStack {
                         Image(systemName: "quote.bubble.fill")
                             .font(.body)
@@ -41,43 +52,20 @@ struct AboutView: View {
                 }
                 
                 // Info
-                Section(header: HStack{
-                            Image(systemName: "questionmark.circle")
-                                .accessibility(label: Text("Information Section"))
-                            Text("Info")}){
-                    NavigationLink(destination: PrivacyView(), label: {
-                        HStack {
-                                Image(systemName: "lock.shield")
-                                    .font(.body)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width:20, alignment: .center)
-                                    .foregroundColor(.green)
-                                    .accessibility(label: Text("Privacy"))
-                                Text("Privacy")
-                        }
-                    })
-                    NavigationLink(destination: TipsView(), label: {
-                        HStack {
-                                Image(systemName: "lightbulb.fill")
-                                    .font(.body)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width:20, alignment: .center)
-                                    .foregroundColor(.yellow)
-                                    .accessibility(label: Text("Tips"))
-                                Text("Tips")
-                        }
-                    })
-                    NavigationLink(destination: InfoView(), label: {
-                        HStack {
-                                Image(systemName: "info.circle.fill")
-                                    .font(.body)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width:20, alignment: .center)
-                                    .foregroundColor(.blue)
-                                    .accessibility(label: Text("About DeTeXt"))
-                                Text("About DeTeXt")
-                        }
-                    })
+                InfoView()
+                
+                // Credits
+                Section(header: Text("Credits").padding(.leading, 20)) {
+                    Text("Thanks to Daniel Kirsch and the team at Detexify for their cool open-source web app that inspired me to make DeTeXt, and for providing the training data and prompt responses to my questions. \n\nThanks to Will Bishop for helping me figure out a tricky problem I had with the PencilKit APIs.\n\nA big thanks to Hans for beta testing new features and giving valuable feedback. \n\nFinally a big thanks to all the folks on the NetNewsWire Slack for being supportive and inspiring me to build my own iOS app.")
+                    Link("Will Bishop", destination: URL(string: "https://willbish.com")!)
+                    Link("Hans", destination: URL(string: "https://twitter.com/SherlockHans")!)
+                    Link("NetNewsWire", destination: URL(string: "https://ranchero.com/netnewswire/")!)
+                }
+                
+                //Privacy
+                Section(header: Text("Privacy").padding(.leading, 20)) {
+                        Text("DeTeXt does not collect or store any personal data or information. All processing of drawings to find the corresponding symbol happens on your device.")
+                    Text("DeTeXt does not display any advertisements, use any trackers or analytics, or send any data to any server.").padding(.bottom, 5)
                 }
             }
             .listStyle(InsetGroupedListStyle())
@@ -85,19 +73,6 @@ struct AboutView: View {
             .navigationTitle("About")
         }
         .navigationViewStyle(StackNavigationViewStyle())
-    }
-}
-
-struct PrivacyView: View {
-    
-    var body: some View {
-        List {
-            Text("DeTeXt does not collect or store any personal data or information. All processing of drawings to find the corresponding symbol happens on your device.")
-            Text("DeTeXt does not display any advertisements, use any trackers or analytics, or send any data to any server.")
-        }
-        .listStyle(InsetGroupedListStyle())
-        
-        .navigationBarTitle("Privacy", displayMode: .inline)
     }
 }
 
