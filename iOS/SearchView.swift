02e9850671a2b684f80b790ad4bc7f98bba9df41
Venkat <gvenkata1994@gmail.com>
2021-10-03 18:27:37 -0500

    searchable modifier for iOS 15

--- iOS/SearchView.swift
+++ iOS/SearchView.swift
@@ -15,84 +15,26 @@ struct SearchView: View {
     @State var showAboutView = false
         
     var body: some View {
-            VStack(spacing: 0) {
-                SearchBar(text: $searchText)
-                    .padding(.top, 8)
-                    .padding(.bottom, 8)
-                Divider()
-                List(symbols.AllSymbols.filter({searchText.isEmpty ? true : ($0.command.lowercased().contains(searchText.lowercased()) || $0.package?.lowercased().contains(searchText.lowercased()) ?? false  )})) { symbol in
-                    RowView(symbol: symbol)
-                        .onDrag { NSItemProvider(object: symbol.command as NSString) }
-                }
-                .listStyle(InsetListStyle())
-            }
-            .navigationBarItems(trailing: Button(action: {self.showAboutView.toggle()}) {
-                    #if targetEnvironment(macCatalyst)
-                        Image(systemName: "questionmark.circle")
-                            .font(.title2)
-                            .accessibility(label: Text("About"))
-                    #else
-                        Image(systemName: "questionmark.circle")
-                            .font(.title3)
-                            .accessibility(label: Text("About"))
-                    #endif
-                }
-            )
-            .navigationTitle("Search")
-            .sheet(isPresented: $showAboutView) { AboutView() }
-    }
-}
-
-struct SearchBar: View {
-    
-    @Binding var text: String
-    @State private var isEditing = false
- 
-    var body: some View {
-        HStack {
-            TextField("Search by command or package", text: $text)
-                .padding(7)
-                .padding(.horizontal, 25)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 8)
-                 
-                        if isEditing {
-                            Button(action: {
-                                self.text = ""
-                            }) {
-                                Image(systemName: "multiply.circle.fill")
-                                    .foregroundColor(.gray)
-                                    .padding(.trailing, 8)
-                            }
-                        }
-                    }
-                )
-                .padding(.horizontal, 10)
-                .onTapGesture {
-                    self.isEditing = true
-                }
-                .disableAutocorrection(true)
- 
-            if isEditing {
-                Button(action: {
-                    self.isEditing = false
-                    self.text = ""
-                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
- 
-                }) {
-                    Text("Cancel")
-                    }
-                    .padding(.trailing, 10)
-                    .transition(.move(edge: .trailing))
-                    .animation(.easeInOut(duration: 0.2))
-            }
+        List(symbols.AllSymbols.filter({searchText.isEmpty ? true : ($0.command.lowercased().contains(searchText.lowercased()) || $0.package?.lowercased().contains(searchText.lowercased()) ?? false  )})) { symbol in
+            RowView(symbol: symbol)
+                .onDrag { NSItemProvider(object: symbol.command as NSString) }
         }
+        .listStyle(InsetListStyle())
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        .navigationBarItems(trailing: Button(action: {self.showAboutView.toggle()}) {
+                #if targetEnvironment(macCatalyst)
+                    Image(systemName: "questionmark.circle")
+                        .font(.title2)
+                        .accessibility(label: Text("About"))
+                #else
+                    Image(systemName: "questionmark.circle")
+                        .font(.title3)
+                        .accessibility(label: Text("About"))
+                #endif
+            }
+        )
+        .navigationTitle("Search")
+        .sheet(isPresented: $showAboutView) { AboutView() }
     }
 }
 
